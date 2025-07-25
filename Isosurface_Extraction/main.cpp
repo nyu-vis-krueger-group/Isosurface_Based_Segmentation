4f456a79f55416b1fe33223402a10d3beec969cd
Chahat Kalsi <chahat.ck88@gmail.com>
2025-07-25 09:08:57 -0400

    run isovalue detection in 2 modes

--- Isosurface_Extraction/main.cpp
+++ Isosurface_Extraction/main.cpp
@@ -1,25 +1,43 @@
 #include <IO/ReadTIFF.h>
+#include <IsosurfaceExtraction/FlyingEdges3D.h>
 #include <Processing/CummulativeLaplacianHistogram.h>
+#include <IO/WritePLY.h>
 #include <windows.h>
 
 /*
-* TODO:
+* Usage: ./IsosurfaceExtraction.exe inputTiff.tiff mode[DETECT/EXTRACT]
 * Run in two modes: 
-* Mode 1: Only detect isovalue and output (right now)
-* Mode 2: Detect isovalue and extract isosurface (https://github.com/Chahat08/BioVis25/blob/7a4f0026de65947eb99365f1d88b03311ebd3d1e/Isosurface_Extraction/main.cpp)
+* Mode 1: (DETECT) (default) Only detect isovalue and output (right now)
+* Mode 2: (EXTRACT) Detect isovalue and extract isosurface (https://github.com/Chahat08/BioVis25/blob/7a4f0026de65947eb99365f1d88b03311ebd3d1e/Isosurface_Extraction/main.cpp)
 */
 int main(int argc, char* argv[]) {
 
-	std::string inputVTI = argv[1];
+	std::string inputTIFF = argv[1];
 
-	ReadTIFF tiffReader(inputVTI);
+	// read input tiff
+	ReadTIFF tiffReader(inputTIFF);
 	tiffReader.read();
 
+
+	// detect isovalue
 	CummulativeLaplacianHistogram histogramCalculator(tiffReader.getOutputPort(), tiffReader.getOutput());
 	std::unordered_map<std::string, std::vector<double>> histogram = histogramCalculator.calculate();
 	int isovalue = histogramCalculator.getKneeIsovalue();
 
 	std::cout << isovalue;
+
+	std::string mode = argv[2];
+	if (!mode.compare("EXTRACT")) {
+		// extract contour
+		FlyingEdges3D contourExtractor(tiffReader.getOutputPort());
+		contourExtractor.setIsoValue(0, isovalue);
+		vtkPolyData* contour = contourExtractor.getOutput();
+
+		// Write the extracted contour as OBJ
+		std::cout << "Writing the extracted contours to " + inputTIFF + "_output.ply" << std::endl;
+		WritePLY plyWriter(inputTIFF + "_output.ply");
+		plyWriter.write(contourExtractor.getOutput());
+	}
 
 	return 0;
 }

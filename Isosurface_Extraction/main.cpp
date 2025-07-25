869be1d70291ccba7b5dbefcfacb42ccc3783312
Chahat Kalsi <chahat.ck88@gmail.com>
2025-07-25 09:01:30 -0400

    run in two modes

--- Isosurface_Extraction/main.cpp
+++ Isosurface_Extraction/main.cpp
@@ -2,7 +2,14 @@
 #include <Processing/CummulativeLaplacianHistogram.h>
 #include <windows.h>
 
+/*
+* TODO:
+* Run in two modes: 
+* Mode 1: Only detect isovalue and output (right now)
+* Mode 2: Detect isovalue and extract isosurface (https://github.com/Chahat08/BioVis25/blob/7a4f0026de65947eb99365f1d88b03311ebd3d1e/Isosurface_Extraction/main.cpp)
+*/
 int main(int argc, char* argv[]) {
+
 	std::string inputVTI = argv[1];
 
 	ReadTIFF tiffReader(inputVTI);
